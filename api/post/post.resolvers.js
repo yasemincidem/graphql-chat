21011978df195cee3acc73029efef9a8d7efb196
yasemin.cidem@sciencelogic.com <yasemin.cidem@sciencelogic.com>
2020-10-03 08:54:01 +0300

    added subscription type

--- api/post/post.resolvers.js
+++ api/post/post.resolvers.js
@@ -1,19 +1,32 @@
 const sendMessage = async (_, { input }, ctx) => {
   const { to, from, text } = input;
-  let result;
+  let newMessage;
   if (!ctx.models.post) {
     result = await ctx.models.post.create({
-        text,
-        from,
-        created_at: new Date().getTime(),
-        to,
-      });
+      text,
+      from,
+      created_at: new Date().getTime(),
+      to,
+    });
   }
-  result = await ctx.models.post.insertMany({ to, text, from, created_at: new Date().getTime() });
-  return result;
+  newMessage = await ctx.models.post.insertMany({
+    to,
+    text,
+    from,
+    created_at: new Date().getTime(),
+  });
+  ctx.pubsub.publish('SEND_MESSAGE_TRIGGER', { newMessage: newMessage[0] });
+  return newMessage;
 };
 module.exports = {
   Mutation: {
     sendMessage,
+  },
+  Subscription: {
+    newMessage: {
+      subscribe(_, args, { pubsub }) {
+        return pubsub.asyncIterator('SEND_MESSAGE_TRIGGER');
+      },
+    },
   },
 };
